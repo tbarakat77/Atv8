c6396575c6053e059f6f2376ca4bfe079eb547d0
Thariq <thariqbarakat37@gmail.com>
2024-04-05 00:12:25 -0300

    Adicionei a formula do IMC

--- Program.cs
+++ Program.cs
@@ -2,9 +2,16 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-	
-	}
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CalculateBMI(75, 1.7));
+        Console.ReadLine();
+    }
 
+    // Calculates the BMI based on a weight and height.
+    static double CalculateBMI(double weight, double height)
+    {
+        double bmi = weight / (height * height);
+        return bmi;
+    }
 }
